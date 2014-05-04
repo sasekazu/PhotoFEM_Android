723ae73d3a3cbe7507b3e60829d9efab640db479
Kazuya Sase <sasekazu@gmail.com>
2014-05-04 17:38:25 +0900

    initial commit.

---
+++ PhotoFEM_Android/src/info/sasekazu/photofem_android/Main.java
@@ -0,0 +1,13 @@
+package info.sasekazu.photofem_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
